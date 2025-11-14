a5b6271e3a308110dc08522b9b87951eae232f2a
joao1barbosa <joao1barbosa@users.noreply.github.com>
2025-11-14 02:24:31 +0000

    deploy: e6dc22378fcb27faba4728a3a389a754f5445c74

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('theme-toggle');
+    const toggleThemeButton = document.getElementById('theme-toggle');
+    const toggleLangButton = document.getElementById('lang-toggle');
     const body = document.body;
-
-    const moonIcon = document.getElementById('moon-icon');
-    const sunIcon = document.getElementById('sun-icon');
+    const html = document.html;
 
     function applyTheme(isDark) {
         if (isDark) {
@@ -15,15 +14,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function applyLanguage(isPortuguese) {
+        if (isPortuguese) {
+            html.classList.add('pt-BR');
+            localStorage.setItem('language', 'en');
+        } else {
+            html.classList.remove('pt-BR');
+            localStorage.setItem('language', 'pt');
+        }
+    }
+
     const savedTheme = localStorage.getItem('theme');
+    const savedLang = localStorage.getItem('language');
 
     if (savedTheme === 'dark') applyTheme(true);
+    if (savedLang === 'pt') applyLanguage(true);
 
-    if (toggleButton) {
-        toggleButton.addEventListener('click', () => {
+    if (toggleThemeButton) {
+        toggleThemeButton.addEventListener('click', () => {
             const isCurrentlyDark = body.classList.contains('dark-mode');
-            // Aplica o tema oposto
             applyTheme(!isCurrentlyDark); 
+        });
+    }
+
+    if (toggleLangButton) {
+        toggleLangButton.addEventListener('click', () => {
+            const isCurrentlyPt = body.classList.contains('pt');
+            applyLanguage(!isCurrentlyPt); 
         });
     }
 });
